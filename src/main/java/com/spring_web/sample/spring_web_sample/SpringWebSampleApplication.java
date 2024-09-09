f57e6b609f9a47a999d487e8ea7bead9eeb486ca
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-09-09 21:25:13 +0530

    web project init

---
+++ src/main/java/com/spring_web/sample/spring_web_sample/SpringWebSampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring_web.sample.spring_web_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_web/sample/spring_web_sample/SpringWebSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_web.sample.spring_web_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
